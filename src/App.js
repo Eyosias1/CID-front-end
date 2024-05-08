8d7bbab8358a5d75ac28a1ef4a41c73952854fb0
Eyosias Woldemichael <eyosias16@gmail.com>
2024-05-08 12:01:42 +0200

    added hero section and Cardsection for info

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar';
-
+import Hero from './components/Hero';
+import InfoCard from './components/InfoCard'; // Import the InfoCard component
 
 function App() {
   return (
     <div className="App">
         <Navbar />
+        <Hero />
+        <InfoCard />
     </div>
   );
 }
