be8b3a9b65b61a2168cae508c5bf5b6e1d4e3ae1
Eyosias Woldemichael <eyosias16@gmail.com>
2024-05-08 21:23:10 +0200

    new payment link

--- src/components/PaymentComponent.js
+++ src/components/PaymentComponent.js
@@ -11,9 +11,15 @@ function PaymentComponent() {
                     publishable-key="pk_live_51PC6XD08xYl0mIJxIQ5biipJpv9j4Ll8m9fjTMRWnTEt7WfkykEYcdxUK0D0gPjW74HbWV4cmDkSESQIBgs7FEPe00p2rsXToh">
                 </stripe-buy-button> */}
 
-                    <stripe-buy-button
+                    {/* <stripe-buy-button
                         buy-button-id="buy_btn_1PC7Ba08xYl0mIJxwAK4pt3l"
                         publishable-key="pk_live_51PC6XD08xYl0mIJxIQ5biipJpv9j4Ll8m9fjTMRWnTEt7WfkykEYcdxUK0D0gPjW74HbWV4cmDkSESQIBgs7FEPe00p2rsXToh"
+                    >
+                    </stripe-buy-button> */}
+
+                    <stripe-buy-button
+                            buy-button-id="buy_btn_1PEG3B08xYl0mIJxsREeD5oh"
+                            publishable-key="pk_live_51PC6XD08xYl0mIJxIQ5biipJpv9j4Ll8m9fjTMRWnTEt7WfkykEYcdxUK0D0gPjW74HbWV4cmDkSESQIBgs7FEPe00p2rsXToh"
                     >
                     </stripe-buy-button>
             </div>
